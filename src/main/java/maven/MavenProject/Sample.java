e039e2bfdf194a55c5e8b22196c22e153f65c82e
akshathakgowda <38320265+akshathakgowda@users.noreply.github.com>
2018-04-13 16:09:25 +0530

    Changes

--- src/main/java/maven/MavenProject/Sample.java
+++ src/main/java/maven/MavenProject/Sample.java
@@ -4,9 +4,13 @@ import org.testng.annotations.Test;
 
 public class Sample
 {
-   @Test
-	public void method()
-	{
-		System.out.println("Hello");
+//   @Test
+//	public void method()
+//	{
+//		System.out.println("Hello");
+//	}
+	
+	public static void main(String[] args) {
+		System.out.println("hello");
 	}
 }
