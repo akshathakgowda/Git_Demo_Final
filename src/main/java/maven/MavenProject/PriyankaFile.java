653af539e9901c26938690292665260fb31a75bc
akshathakgowda <38320265+akshathakgowda@users.noreply.github.com>
2018-04-17 15:08:28 +0530

    done

---
+++ src/main/java/maven/MavenProject/PriyankaFile.java
@@ -0,0 +1,13 @@
+package maven.MavenProject;
+
+import org.testng.annotations.Test;
+
+public class PriyankaFile
+{
+
+	@Test
+	public void me()
+	{
+		System.out.println("Priyanka is funny");
+	}
+}
